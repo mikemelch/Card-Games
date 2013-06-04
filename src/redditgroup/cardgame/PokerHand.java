44fa9f2d44a55c939d8e433e0a6fc90eeff18d8f
ydd <Wnt2bsleepin@gmail.com>
2013-06-04 14:29:30 -0400

    Added enums to card class, changed packages and other small mods to the card system

---
+++ src/redditgroup/cardgame/PokerHand.java
@@ -0,0 +1,521 @@
+package redditgroup.cardgame;
+
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
+
+public class PokerHand extends Hand{
+	
+	public PokerHand(){
+		super();
+	}
+	
+	public int[] PokerValue(){
+		int [] pv = new int[7];
+		int curr = 1;
+		int clubs = 0, spades = 0, hearts = 0, diamonds = 0;
+		int s = 0;
+		boolean straight = false, flush = false;
+		Card temp[] = new Card[hand.size()];
+		int n = temp.length;
+		
+		for(int i = 0; i < hand.size(); i++){
+			temp[i] = hand.get(i);
+		}
+		
+		for (int pass=1; pass < n; pass++) { 
+	        for (int i=0; i < n-pass; i++) {
+	            if (temp[i].getCardNumber() > temp[i+1].getCardNumber()) {
+	                Card t = temp[i];  
+	                temp[i] = temp[i+1];  
+	                temp[i+1] = t;
+	            }
+	        }
+	    }
+
+		for(int i = 0; i < hand.size(); i++){
+			if(getCard(i).getSuit().equals("Clubs")){
+				clubs++;
+			}
+			else if(getCard(i).getSuit().equals("Spades")){
+				spades++;
+			}
+			else if(getCard(i).getSuit().equals("Hearts")){
+				hearts++;
+			}
+			else
+				diamonds++;
+			
+		}
+		
+		if((clubs == 5) || (spades == 5) || (hearts == 5) || (diamonds == 5)){
+			flush = true;
+			pv[1] = temp[hand.size() - 1].getCardNumber();
+			pv[2] = temp[hand.size() - 2].getCardNumber();
+			curr += 2;
+		}
+			
+		
+		for(int i = 0; i < hand.size() - 1; i++){
+			if(temp[i+1].getCardNumber() - temp[i].getCardNumber() == 1){
+				s++;
+			}	
+			else{
+				break;
+			}
+		}	
+		
+		if(s == 4){
+			straight = true;
+			pv[1] = temp[hand.size() - 1].getCardNumber();
+			pv[2] = temp[hand.size() - 2].getCardNumber();
+			curr += 2;
+		}
+		
+		boolean pair = false, twopair = false, three = false, fullhouse = false, fourkind = false;
+		for(int i = 1; i <= 13; i++){
+			int count = 0;
+			for(Card c : hand){
+				if(c.getCardNumber() == i){
+					count++;
+				}
+			}
+			if(count == 2){
+				if(pair){
+					twopair = true;
+					pv[curr] = i;
+					curr++;
+				}
+				else if(three){
+					fullhouse = true;
+					pv[curr] = i;
+					curr++;
+				}
+				else{
+					pair = true;
+					pv[curr] = i;
+					curr++;
+				}
+			}
+			else if(count == 3){
+				if(pair){
+					fullhouse = true;
+					pv[curr] = i;
+					curr++;
+				}
+				else{
+					three = true;
+					pv[curr] = i;
+					curr++;
+				}
+			}
+			else if(count == 4){
+				fourkind = true;
+				pv[curr] = i;
+				curr++;
+			}
+			
+		}
+		
+		if((straight) && (flush)){
+			pv[0] = 10;
+		}
+		else if(fourkind){
+			pv[0] = 9;
+		}
+		else if(fullhouse){
+			pv[0] = 8;
+		}
+		else if(flush){
+			pv[0] = 7;
+		}
+		else if(straight){
+			pv[0] = 6;
+		}
+		else if(three){
+			pv[0] = 5;
+		}
+		else if(twopair){
+			pv[0] = 4;
+		}
+		else if(pair){
+			pv[0] = 3;
+		}
+		else
+			pv[0] = 2;
+		
+		return pv;
+
+	}
+
+
+	public String showPokerValue(){
+		if(PokerValue()[0] == 10){
+			return "Straight Flush";
+		}
+		else if(PokerValue()[0] == 9){
+			return "Four of a Kind";
+		}
+		else if(PokerValue()[0]== 8){
+			return "Full House";
+		}
+		else if(PokerValue()[0] == 7){
+			return "Flush";
+		}
+		else if(PokerValue()[0] == 6){
+			return "Straight";
+		}
+		else if(PokerValue()[0] == 5){
+			return "Three of a Kind";
+		}
+		else if(PokerValue()[0] == 4){
+			return "Two Pair";
+		}
+		else if(PokerValue()[0] == 3){
+			return "Pair";
+		}
+		else{
+			return "Higher Card";
+		}
+		
+	}
+
+	public void dealerExchange(Deck d, JLabel[] dc){
+		ArrayList<Card> keepcards = new ArrayList<Card>();
+		
+		if(PokerValue()[0] == 10 || PokerValue()[0] == 8 || PokerValue()[0] == 7 || PokerValue()[0] == 6){
+			return;
+		}
+		else if(PokerValue()[0] == 9){
+			int val = 0;
+			if(hand.get(0).getCardNumber() == hand.get(1).getCardNumber()){
+				val = hand.get(0).getCardNumber();
+			}
+				
+			if(hand.get(3).getCardNumber() == hand.get(4).getCardNumber()){
+				val = hand.get(0).getCardNumber();
+			}
+			Card c = null;
+			
+			for(int i = 0; i < 5; i++){
+				c = hand.get(i);	
+				
+				if(c.getCardNumber() == val){
+					keepcards.add(c);
+				}
+				else if(c.getCardNumber() < 9){
+					keepcards.add(d.dealCard());
+				}
+				else{
+					keepcards.add(c);
+				}
+			}
+		}
+		
+		else if(PokerValue()[0] == 5){
+			int val = 0;
+			if(hand.get(0).getCardNumber() == hand.get(1).getCardNumber()){
+				val = hand.get(0).getCardNumber();
+			}
+				
+			else if(hand.get(3).getCardNumber() == hand.get(4).getCardNumber()){
+				val = hand.get(0).getCardNumber();
+			}
+			else if(hand.get(2).getCardNumber() == hand.get(3).getCardNumber()){
+				val = hand.get(0).getCardNumber();
+			}
+			if(hand.get(0).getCardNumber() == hand.get(4).getCardNumber()){
+				val = hand.get(0).getCardNumber();
+			}
+			
+			Card c = null;
+			
+			for(int i = 0; i < 5; i++){
+				c = hand.get(i);	
+				
+				if(c.getCardNumber() == val){
+					keepcards.add(c);
+				}
+				else{
+					keepcards.add(d.dealCard());
+				}
+			}
+		}
+		
+		else if(PokerValue()[0] == 4){
+			int val1 = -1;
+			int val2 = -1;
+			
+			for(int i = 0; i < 5; i++){
+				Card c = hand.get(i);
+				for(int j = 0; j < 5; j++){
+					Card a = hand.get(j);
+					
+					if(c.getCardNumber() == a.getCardNumber() && c != a){
+						if(val1 == -1){
+							val1 = c.getCardNumber();
+						}
+						else{
+							val2 = c.getCardNumber();
+						}
+					}
+				}
+			}
+			
+			for(int i = 0; i < 5; i++){
+				Card b = hand.get(i);
+				
+				if(b.getCardNumber() == val1 || b.getCardNumber() == val2){
+					keepcards.add(b);
+				}
+				else{
+					keepcards.add(d.dealCard());
+				}
+			}
+		}
+		
+		else if(PokerValue()[0] == 3){
+			int val = -1;
+			
+			for(int i = 0; i < 5; i++){
+				Card c = hand.get(i);
+				for(int j = 0; j < 5; j++){
+					Card a = hand.get(j);
+					
+					if(c.getCardNumber() == a.getCardNumber() && c != a){
+						val = c.getCardNumber();
+						break;
+					}
+				}
+			}
+			
+			for(int i = 0; i < 5; i++){
+				Card b = hand.get(i);
+				
+				if(b.getCardNumber() == val){
+					keepcards.add(b);
+				}
+				else{
+					keepcards.add(d.dealCard());
+				}
+			}
+		}
+		
+		else{
+			int clubs = 0, spades = 0, diamonds = 0, hearts = 0;
+			int n = hand.size();
+			ArrayList<Card> temp = hand;
+			
+			for (int pass=1; pass < n; pass++) { 
+		        for (int i=0; i < n-pass; i++) {
+		            if (temp.get(i).getCardNumber() > temp.get(i + 1).getCardNumber()) {
+		                Card t = temp.get(i);  
+		                temp.set(i, temp.get(i + 1));  
+		                temp.set(i + 1, t);
+		            }
+		        }
+		    }
+		    
+			for(int i = 0; i < 5; i++){
+				if(getCard(i).getSuit().equals("Clubs")){
+					clubs++;
+					if(clubs >= 3){
+						for(int j = 0; j < 5; j++){
+							if(hand.get(j).getSuit().equals("Clubs")){
+								keepcards.add(hand.get(j));
+							}
+							else
+								keepcards.add(d.dealCard());
+						}
+					}
+				}
+				else if(getCard(i).getSuit().equals("Spades")){
+					spades++;
+					if(spades >= 3){
+						for(int j = 0; j < 5; j++){
+							if(hand.get(j).getSuit().equals("Spades")){
+								keepcards.add(hand.get(j));
+							}
+							else
+								keepcards.add(d.dealCard());
+						}
+					}
+				}
+				else if(getCard(i).getSuit().equals("Hearts")){
+					hearts++;
+					if(hearts >= 3){
+						
+						for(int j = 0; j < 5; j++){
+							if(hand.get(j).getSuit().equals("Hearts")){
+								keepcards.add(hand.get(j));
+							}
+							else
+								keepcards.add(d.dealCard());
+						}
+					}
+				}
+				else{
+					diamonds++;
+					if(diamonds >= 3){
+						for(int j = 0; j < 5; j++){
+							if(hand.get(j).getSuit().equals("Diamonds")){
+								keepcards.add(hand.get(j));
+							}
+							else
+								keepcards.add(d.dealCard());
+						}
+					}
+				}
+				
+			}
+			if(keepcards.size() == 0){
+				int s = 0;
+				ArrayList<Card> st = new ArrayList<Card>();
+				
+				for(int i = 0; i < 4; i++){
+					if(temp.get(i + 1).getCardNumber() - temp.get(i).getCardNumber() == 1){
+						s++;
+						if(!st.contains(temp.get(i))){
+							st.add(temp.get(i));
+						}
+						if(!st.contains(temp.get(i + 1))){
+							st.add(temp.get(i + 1));
+						}
+						
+					}
+
+				}	
+				
+				if(s >= 2 && st.get(st.size() - 1).getCardNumber() - st.get(0).getCardNumber() < 4){
+					keepcards = st;
+					for(int i = keepcards.size(); i <= 5; i++){
+						keepcards.add(d.dealCard());
+					}
+				}
+				
+			}
+			
+			if(keepcards.size() == 0){
+				/**
+				 * Keep 2 highest values
+				 */
+				keepcards.add(temp.get(3));
+				keepcards.add(temp.get(4));
+				
+				
+				keepcards.add(d.dealCard());
+				keepcards.add(d.dealCard());
+				keepcards.add(d.dealCard());
+			}
+
+		}
+		
+		
+		clearHand();
+		for(int i = 0; i < 5; i++){
+			hand.add(keepcards.get(i));
+		}
+		
+	}
+	
+	public int bet(int amount){
+		int pv = PokerValue()[0];
+		if(amount >= 500){
+			if(pv < 4){
+				return -1;
+			}
+			else if(pv < 7){
+				return amount;
+			}
+			else{
+				return amount + 100;
+			}
+		}
+		
+		else if(amount >= 100){
+			if(pv < 3){
+				return -1;
+			}
+			else if(pv < 7){
+				return amount + 100;
+			}
+			else{
+				return amount * 2;
+			}
+		}
+		else if(amount >= 10){
+			if(pv < 3){
+				return amount;
+			}
+			else if(pv < 7){
+				return amount * 2;
+			}
+			else{
+				return amount * 5;
+			}
+		}
+		else{
+			if(pv < 3){
+				return amount;
+			}
+			else if(pv < 7){
+				return amount + 100;
+			}
+			else{
+				return amount + 700;
+			}
+		}
+
+	}
+	
+	public int higherHand(PokerHand dh){
+		
+		Card temp[] = new Card[hand.size()];
+		int n = temp.length;
+		
+		for(int i = 0; i < hand.size(); i++){
+			temp[i] = hand.get(i);
+		}
+		
+		for (int pass=1; pass < n; pass++) { 
+	        for (int i=0; i < n-pass; i++) {
+	            if (temp[i].getCardNumber() > temp[i+1].getCardNumber()) {
+	                Card t = temp[i];  
+	                temp[i] = temp[i+1];  
+	                temp[i+1] = t;
+	            }
+	        }
+	    }
+		
+		Card temp2[] = new Card[dh.hand.size()];
+		int num = temp2.length;
+		
+		for(int i = 0; i < dh.hand.size(); i++){
+			temp2[i] = dh.hand.get(i);
+		}
+		
+		for (int pass=1; pass < num; pass++) { 
+	        for (int i=0; i < num-pass; i++) {
+	            if (temp2[i].getCardNumber() > temp2[i+1].getCardNumber()) {
+	                Card t = temp2[i];  
+	                temp2[i] = temp2[i+1];  
+	                temp2[i+1] = t;
+	            }
+	        }
+	    }
+		
+		for(int i = 0; i < temp.length; i++){
+			if(temp[i].getCardNumber() > temp2[i].getCardNumber()){
+				return -1;
+			}
+			else if(temp[i].getCardNumber() < temp2[i].getCardNumber()){
+				return 1;
+			}
+			else{
+				continue;
+			}
+		}
+		
+		return 0;
+	}
+}
---
+++ src/redditgroup/cardgame/VisualBJ.java
@@ -0,0 +1,317 @@
+package redditgroup.cardgame;
+///////////////////////////////////
+//Michael Melchione
+//AP Computer Science A
+//6-1-12
+//VisualBJ
+///////////////////////////////////
+
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class VisualBJ extends JFrame implements ActionListener{
+	
+	JButton hit;
+	JButton stay;
+	JButton deal;
+	JButton doubledown;
+	int amount;
+	JLabel dc;
+	JLabel yc;
+	JTextField bettf;
+	JButton bet;
+	JTextField results;
+	JLabel[] dealercards;
+	JLabel[] playercards;
+	JPanel dealer;
+	JPanel buttons;
+	JPanel player;
+	Deck deck;
+	BJHand playerHand;
+	BJHand dealerHand;
+	JPanel buttons2;
+	Icon iconName;
+	int i, j;
+	boolean win, lose;
+	boolean push, down;
+	
+	public VisualBJ(String name){
+		super(name);
+		setBackground(new Color(0, 100, 0));
+		setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+		setSize(500, 345); 	
+		hit = new JButton("Hit");
+		stay = new JButton("Stay");
+		deal = new JButton("Deal");
+		doubledown = new JButton("Double Down");
+		bettf = new JTextField("-enter amount-");
+		bet = new JButton("Bet!");
+		dc = new JLabel("Dealer:");
+		yc = new JLabel("Player:");
+		results = new JTextField(50);
+		dealercards = new JLabel[7];
+		playercards = new JLabel[7];
+		dealer = new JPanel();
+		buttons = new JPanel();
+		buttons2 = new JPanel();
+		player = new JPanel();
+		dealer.add(dc);
+		player.add(yc);
+		i = 2;
+		j = 2;
+		win = false;
+		push = false;
+		lose = false;
+		iconName = new ImageIcon();
+		for(int i = 0; i <= 6; i++){
+			dealercards[i] = new JLabel();
+			playercards[i] = new JLabel();
+			dealer.add(dealercards[i]);
+			player.add(playercards[i]);
+		}
+	
+		buttons.add(hit);
+		buttons.add(stay);
+		buttons.add(deal);
+		buttons2.add(bet);
+		buttons2.add(bettf);
+		buttons2.add(doubledown);
+		bet.setActionCommand("Bet");
+		bet.addActionListener(this);
+		add(dealer);
+		add(buttons);
+		add(buttons2);
+		add(player);
+		add(results);
+		
+		hit.setActionCommand("hit");
+		stay.setActionCommand("stay");
+		deal.setActionCommand("deal");
+		doubledown.setActionCommand("down");
+		
+		hit.addActionListener(this);
+		stay.addActionListener(this);
+		deal.addActionListener(this);
+		doubledown.addActionListener(this);
+		
+		hit.setEnabled(false);
+		stay.setEnabled(false);
+		bet.setEnabled(true);
+		bettf.setEnabled(true);
+		doubledown.setEnabled(false);
+		deal.setEnabled(false);
+		
+		playerHand = new BJHand();
+		dealerHand = new BJHand();
+		deck = new Deck();
+	}
+	
+	public void actionPerformed(ActionEvent evt) {
+		String event = evt.getActionCommand();
+		
+		if(event.equals("deal")){
+			bettf.setText("      ");
+			results.setText(null);
+			amount = 0;
+			win = false;
+			lose = false;
+			push = false;
+			i = 2;
+			j = 2;
+			for(int k = 0; k < 7; k++){
+				dealercards[k].setIcon(null);
+				playercards[k].setIcon(null);
+			}
+			results.setText("");
+			deck.shuffle();
+
+			
+			hit.setEnabled(false);
+			stay.setEnabled(false);
+			deal.setEnabled(false);
+			doubledown.setEnabled(false);
+			bet.setEnabled(true);
+			bettf.setEnabled(true);
+
+		}
+		
+		else if(event.equals("hit")){
+			doubledown.setEnabled(false);
+			playerHand.addCard(deck.dealCard());
+			iconName = new ImageIcon(playerHand.getCard(i).getFileName());
+			playercards[i].setIcon(iconName);
+			i++;
+			if(i == 6)
+			{
+				results.setText("Five Card Charlie! You win $" + amount + "!");
+				win = true;
+			}
+			
+			if(playerHand.BJValue() > 21){
+				results.setText("You busted! Dealer wins! You lose $" + amount + "!");
+				lose = true;
+			}
+			
+		}
+		
+		else if(event.equals("stay")){
+			doubledown.setEnabled(false);
+            iconName = new ImageIcon(dealerHand.getCard(0).getFileName());
+			dealercards[0].setIcon(iconName);
+			
+			while(dealerHand.BJValue() < 17)
+			{
+				
+				dealerHand.addCard(deck.dealCard());
+				iconName = new ImageIcon(dealerHand.getCard(j).getFileName());
+				dealercards[j].setIcon(iconName);
+				j++;
+			}
+			
+			if(dealerHand.BJValue() > 21)
+			{
+				results.setText("The dealer busted. You win $" + amount + "!");
+				win = true;
+			}
+			else if(dealerHand.BJValue() > playerHand.BJValue())
+			{
+				results.setText("The dealer has a better hand than you. You lose $" + amount + "!");
+				lose = true;
+			}
+			else if((dealerHand.BJValue() < playerHand.BJValue()))
+			{
+				results.setText("You have a better hand than the dealer. You win $" + amount + "!");
+				win = true;
+			}
+			else
+			{
+				results.setText("You and the dealer have the same hand. You push.");
+				push = true;
+			}
+		}
+		
+		if(event.equals("down")){
+			hit.setEnabled(false);
+			deal.setEnabled(false);
+			stay.setEnabled(false);
+			amount = Integer.parseInt(bettf.getText().trim());
+			bettf.setEnabled(false);
+			bet.setEnabled(false);
+			
+			doubledown.setEnabled(false);
+			playerHand.addCard(deck.dealCard());
+			iconName = new ImageIcon(playerHand.getCard(i).getFileName());
+			playercards[i].setIcon(iconName);
+			i++;
+			
+			if(playerHand.BJValue() > 21){
+				results.setText("You busted! Dealer wins!");
+				results.setText(results.getText() + "    You lose $" + amount * 2 + "!");
+				lose = true;
+			}
+			else{
+				iconName = new ImageIcon(dealerHand.getCard(0).getFileName());
+				dealercards[0].setIcon(iconName);
+				
+				while(dealerHand.BJValue() < 17)
+				{
+					
+					dealerHand.addCard(deck.dealCard());
+					iconName = new ImageIcon(dealerHand.getCard(j).getFileName());
+					dealercards[j].setIcon(iconName);
+					j++;
+				}
+				
+				if(dealerHand.BJValue() > 21)
+				{
+					results.setText("The dealer busted. You win $" + amount * 2 + "!");
+					win = true;
+				}
+				else if(dealerHand.BJValue() > playerHand.BJValue())
+				{
+					results.setText("The dealer has a better hand than you. You lose $" + amount * 2 + "!");
+					lose = true;
+				}
+				else if((dealerHand.BJValue() < playerHand.BJValue()))
+				{
+					results.setText("You have a better hand than the dealer. You win $" + amount * 2 + "!");
+					win = true;
+				}
+				else
+				{
+					results.setText("You and the dealer have the same hand. You push.");
+					push = true;
+				}
+				
+			}
+		}
+
+		else if(event.equals("Bet")){
+			amount = Integer.parseInt(bettf.getText().trim());
+			deck.shuffle();
+			if(playerHand.BJValue() == 21){
+				results.setText("BLACKJACK! You win! You win $" + amount * 3 + "!");
+				win = true;
+				hit.setEnabled(false);
+				stay.setEnabled(false);
+				deal.setEnabled(true);
+				return;
+			}
+			else if(dealerHand.BJValue() == 21){
+				results.setText("BLACKJACK! Dealer wins! You lose $" + amount + "!");
+				iconName = new ImageIcon(dealerHand.getCard(0).getFileName());
+				dealercards[0].setIcon(iconName);
+				lose = true;
+				hit.setEnabled(false);
+				stay.setEnabled(false);
+				deal.setEnabled(true);
+				return;
+			}
+			playerHand.clearHand();
+			dealerHand.clearHand();	
+			playerHand.addCard(deck.dealCard());
+			dealerHand.addCard(deck.dealCard());
+			playerHand.addCard(deck.dealCard());
+			dealerHand.addCard(deck.dealCard());
+			iconName = new ImageIcon("b.gif");
+			dealercards[0].setIcon(iconName);
+			iconName = new ImageIcon(dealerHand.getCard(1).getFileName());
+			dealercards[1].setIcon(iconName);
+			iconName = new ImageIcon(playerHand.getCard(0).getFileName());
+			playercards[0].setIcon(iconName);
+			iconName = new ImageIcon(playerHand.getCard(1).getFileName());
+			playercards[1].setIcon(iconName);
+			doubledown.setEnabled(true);
+			hit.setEnabled(true);
+			stay.setEnabled(true);
+			
+			
+			bet.setEnabled(false);
+			bettf.setEnabled(false);
+			
+		}
+		
+		if((win) || (lose) || (push)){
+			hit.setEnabled(false);
+			stay.setEnabled(false);
+			deal.setEnabled(true);
+		}
+		
+	}
+	
+	public static void main(String[] args){
+		VisualBJ window = new VisualBJ("Blackjack");
+		window.show();
+	}
+}
